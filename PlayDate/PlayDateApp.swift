5f03415677fab87a068a65a5cbcb06d7c11c802c
Pattapon Gowanit <iampattapon@gmail.com>
2026-05-09 14:52:21 +0700

    Add Xcode project scaffold and MVVM folder structure

---
+++ PlayDate/PlayDateApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayDateApp.swift
+//  PlayDate
+//
+//  Created by Pattapon Gowanit on 9/5/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct PlayDateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
